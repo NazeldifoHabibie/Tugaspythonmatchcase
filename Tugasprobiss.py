f7233615e2ccdae0f1eb575b9d70a9993bdb515a
Nazeldifo Habibie <nazeldifohabibie@gmail.com>
2026-04-28 09:29:37 +0700

    Add files via upload

---
+++ Tugasprobiss.py
@@ -0,0 +1,20 @@
+print("A01 A02 B01 B02 C01")
+Kode = (input("Pilih"))
+match Kode:
+  case "A01":
+    print("Barang : Snack")
+    print("Harga  : 100000")
+  case "A02":
+    print("Barang : Minuman")
+    print("Harga  : 8000")
+  case "B01":
+    print("Barang : Mie Instan")
+    print("Harga  : 3000")
+  case "B02":
+    print("Barang : Sabun")
+    print("Harga  : 5000")
+  case "C01":
+    print("Barang : Shampo")
+    print("Harga  : 12000")
+    
+
